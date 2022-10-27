fea1ebb80152548f46f96e58a10d4d80919ac7e1
asolonevich <asolonevich@mail.ru>
2022-10-28 00:28:18 +0300

    adeted seting.py and done collectsatic

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,9 +134,6 @@ LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-#Herocu: обновление конфигурации БД из $DATABASE_URL
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # easy work statics files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
