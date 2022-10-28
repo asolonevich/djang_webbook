e81b1319a1546e7120e3e75449a9a6ec39ea6b3f
asolonevich <asolonevich@mail.ru>
2022-10-28 22:05:44 +0300

    modifited setting.py add STATIC

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -123,6 +123,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
